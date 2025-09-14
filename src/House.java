69989d14bef720aa92e3a93a39e58a0bc632ac18
Lattuse <sultik280805@gmail.com>
2025-09-14 16:46:34 +0500

    All needed classes

--- src/House.java
+++ src/House.java
@@ -1,4 +1,86 @@
-package PACKAGE_NAME;
-
 public class House {
+    private final int floors;
+    private final int rooms;
+    private final boolean hasGarage;
+    private final boolean hasGarden;
+    private final boolean hasSwimmingPool;
+
+    // private constructor here
+    private House(Builder builder) {
+        this.floors = builder.floors;
+        this.rooms = builder.rooms;
+        this.hasGarage = builder.hasGarage;
+        this.hasGarden = builder.hasGarden;
+        this.hasSwimmingPool = builder.hasSwimmingPool;
+    }
+
+    // getters
+    public int getFloors() {
+        return floors;
+    }
+
+    public int getRooms() {
+        return rooms;
+    }
+
+    public boolean hasGarage() {
+        return hasGarage;
+    }
+
+    public boolean hasGarden() {
+        return hasGarden;
+    }
+
+    public boolean hasSwimmingPool() {
+        return hasSwimmingPool;
+    }
+
+    @Override
+    public String toString() {
+        return "House{" +
+                "floors=" + floors +
+                ", rooms=" + rooms +
+                ", hasGarage=" + hasGarage +
+                ", hasGarden=" + hasGarden +
+                ", hasSwimmingPool=" + hasSwimmingPool +
+                '}';
+    }
+
+    // builder class
+    public static class Builder {
+        private int floors;
+        private int rooms;
+        private boolean hasGarage;
+        private boolean hasGarden;
+        private boolean hasSwimmingPool;
+
+        public Builder setFloors(int floors) {
+            this.floors = floors;
+            return this;
+        }
+
+        public Builder setRooms(int rooms) {
+            this.rooms = rooms;
+            return this;
+        }
+
+        public Builder setGarage(boolean hasGarage) {
+            this.hasGarage = hasGarage;
+            return this;
+        }
+
+        public Builder setGarden(boolean hasGarden) {
+            this.hasGarden = hasGarden;
+            return this;
+        }
+
+        public Builder setSwimmingPool(boolean hasSwimmingPool) {
+            this.hasSwimmingPool = hasSwimmingPool;
+            return this;
+        }
+
+        public House build() {
+            return new House(this);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        House house = new House.Builder()
+                .setFloors(2)
+                .setRooms(5)
+                .setGarage(true)
+                .setGarden(true)
+                .setSwimmingPool(false)
+                .build();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(house);
     }
 }
+
+
+
+
